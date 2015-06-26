0d82de08f8d67af606935278a8061d5d818d8ebe
Anton Lundin <anton.lundin@softhouse.se>
2015-06-26 09:53:49 +0200

    added controller for profile

--- src/app/components/profile/profile.controller.js
+++ src/app/components/profile/profile.controller.js
@@ -5,12 +5,30 @@
         .module('xyz-cv-ui.profile')
         .controller('ProfileController', ProfileController);
 
-        function ProfileController() {
+        function ProfileController(Skill) {
             var vm = this;
 
             vm.hello = [6, 7, 8, 9, 10];
+            vm.skills = [];
+
+            getSkills();
 
             //////////////
+
+            function getSkills() {
+                vm.skills = Skill.query();
+            }
+
+            function createSkill() {
+                var skill = {
+                    name: 'testSkill'
+                };
+                Skill.save(skill);
+            }
+
+            function deleteSkill() {
+                Skill.remove({id: '558bed98ed289d0f00d2c602'});
+            }
 
             function refresh() {
                 /* */
