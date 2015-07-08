907438e5a0160b887f1abaa47304495611e715f2
Anton Lundin <anton.lundin@softhouse.se>
2015-07-08 18:13:54 +0200

    basic tests

---
+++ src/test/profile.module.test.js
@@ -0,0 +1,11 @@
+describe('xyz-cv-ui.profile', function() {
+
+    var module;
+    before(function() {
+        module = angular.module('xyz-cv-ui.profile');
+    });
+
+    it('should be registered', function() {
+        expect(module).not.to.equal(null);
+    });
+});
