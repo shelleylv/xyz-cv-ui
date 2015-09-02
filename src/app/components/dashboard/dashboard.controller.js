2269db05529c7191f73344093e3d53970933c0f2
Rasmus Letterkrantz <rasmus.letterkrantz@gmail.com>
2015-09-02 12:39:26 +0200

    kenny added new styling for dashboard

--- src/app/components/dashboard/dashboard.controller.js
+++ src/app/components/dashboard/dashboard.controller.js
@@ -5,76 +5,110 @@
         .module('xyz-cv-ui.dashboard')
         .controller('DashboardController', DashboardController);
 
-        function DashboardController(DashboardModel) {
+        function DashboardController(DashboardModel, $filter) {
             var vm = this;
-            window.vm9 = vm;
 
-             DashboardModel.get()
-                .$promise.then(function(model) {
-                    vm.competence = model.competence;
-                    vm.offices = model.offices;
-                    vm.skills = model.skills;
-                    vm.skill = [];
-                    vm.office = [];
-                    vm.getUserNamesInSelectedOfficesCSV = getUserNamesInSelectedOfficesCSV;
-                    vm.getUserSkillCSV = getUserSkillCSV;
-                    vm.setCompetenceData = setCompetenceData;
-                    vm.competenceData = [];
-                    defaultSkillsToValue(true);
-                    vm.activated = true;
-            });
+            vm.competence = {}
+            vm.offices = [];
+            vm.skills = [];
+            vm.skill = [];
+            vm.office = [];
+            vm.getSelectedSkillsCSV = getSelectedSkillsCSV;
+            vm.getUserSkillCSV = getUserSkillCSV;
+            vm.setCompetenceData = setCompetenceData;
+            vm.setAllSkills = setAllSkills;
+            vm.competenceData = [];
+            vm.activated = false;
+
+            activate();
 
             //////////////
 
-            function setCompetenceData() {
-                var resultArr = [];
-                vm.competence.map(function(competence) {
-                    if (vm.skill[competence.skill]) {
-                    var users = [];
-
-                    competence.users.map(function(user) {
-                         if (vm.office[user.office]) {
-                            users.push({'name': user.name, 'level': user.level, 'office': user.office, 'userId': user.userId});
-                         }
-                    });
-                    resultArr.push({'skill': competence.skill, 'users': users});
-                    }
+            function activate() {
+                DashboardModel.get()
+                    .$promise.then(function(model) {
+                        vm.competence = model.competence;
+                        vm.offices = model.offices; // $filter('orderBy')(model.offices);
+                        vm.skills = model.skills;
+                        vm.skills[0].skills = $filter('orderBy')(vm.skills[0].skills);
+                        vm.skill = [];
+                        vm.office = [];
+                        vm.competenceData = [];
+                        vm.activated = true;
                 });
-                vm.competenceData = resultArr;
             }
 
-            function getUserNamesInSelectedOfficesCSV() {
-                var arr = [];
-                var userNames = [''];
-                vm.competenceData[0].users.map(function(user) {
-                    userNames.push(user.name);
-                });
-
-                arr.push(userNames);
-                return arr;
-            }
-
-            function getUserSkillCSV() {
-                var resultArr = [];
-                vm.competenceData.map(function(competence) {
-                    var arr = [];
-                    arr.push(competence.skill);
-
-                    competence.users.map(function(user) {
-                        arr.push(user.level);
-                    });
-
-                    resultArr.push({skills: arr});
-                });
-
-                return resultArr;
-            }
-
-            function defaultSkillsToValue(value) {
+            function setAllSkills(value) {
                 vm.skills[0].skills.map(function(skill) {
                     vm.skill[skill] = value;
                 });
                 setCompetenceData();
+            }
+
+            function setCompetenceData() {
+            	var resultArr = [];
+
+            	// Matrix x-axis name
+				var competenceData = [];
+
+				// Build list with selected skills
+                competenceData.skills = [];
+                vm.competence.map(function(competence) {
+                    if (vm.skill[competence.skill]) {
+	                    competenceData.skills.push(competence.skill);
+                    }
+                });
+                // Sort skills by name
+                competenceData.skills = $filter('orderBy')(competenceData.skills);
+
+                // Build list with all users
+                competenceData.users = [];
+                vm.competence[0].users.map(function(user) {
+                     if (vm.office[user.office]) {
+                     	var newuser = {'name': user.name, 'skills': [], 'office': user.office, 'userId': user.userId};
+                        competenceData.users.push(newuser);
+                        // Create (selected) skills for each user
+                        competenceData.skills.map(function(skill) {
+                        	// Loop through all skills
+                        	vm.competence.map(function(competence) {
+                        		if (competence.skill === skill) {
+                        			// Find user
+                        			competence.users.map(function(testuser) {
+                        				if (testuser.name === user.name) {
+                        					newuser.skills.push({ 'level': testuser.level });
+                        				}
+                        			});
+                        		}
+                        	});
+                        });
+                     }
+                });
+                // Sort users by name
+                competenceData.users = $filter('orderBy')(competenceData.users,'name');
+
+                vm.competenceData = competenceData;
+            }
+
+            function getSelectedSkillsCSV() {
+            	var result = angular.copy(vm.competenceData.skills);
+            	result.unshift('Employee');
+                return result;
+            }
+
+            function getUserSkillCSV() {
+                var resultArr = [];
+
+                vm9.competenceData.users.map(function(user) {
+                	var row = [];
+                	row.push(user.name);
+                	user.skills.map(function(skill) {
+                		row.push(skill.level);
+                	});
+                	resultArr.push(row);
+                });
+
+
+                return resultArr;
             }
 
         }
